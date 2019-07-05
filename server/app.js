d75014c2c6a0d1709276d802a046734882f9e40c
jm-alvarezs <jm.alvarezs98@gmail.com>
2019-07-05 13:14:50 -0500

    separated server from static for deployment. Email send works

---
+++ server/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mailer = require("express-mailer");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mailer.extend(app, {
+    from: "hello@thenorthcode.com",
+    host: 'thenorthcode.com', // hostname
+    secureConnection: true, // use SSL
+    port: 465, // port for secure SMTP
+    transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+    auth: {
+      user: 'hola@thenorthcode.com',
+      pass: '#consu2019'
+    }
+});
+
+app.post("/send", (req, res) => {
+    app.mailer.send("email", {
+        to: "hola@thenorthcode.com",
+        subject: "Hello from Website",
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        message: req.body.message
+    }, (err, message) => {
+        if(err) {
+            res.status(500).send("Error sending email");
+        } else {
+            res.send('ok');
+        }
+    })
+});
+
+app.listen(3000, () => console.log("running"));
